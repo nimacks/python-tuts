70fc2620e4994bb925325018c4de234fe32d04e0
Samuel Mensah <samuelnmensah@live.com>
2017-11-13 12:23:43 -0800

    added helloworld

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+#single line comments
+
+# Division
+print(5//3)
+a = [1,2,34]
+
+hell = "Hello" "World"
+
+
+input_string = input("What is your name ")
+#print(input_string)
+
+message = "{} to the real world of  {}".format("welcome","programming")
+print(message)
+
+#Lists
+
+li = [9,3,2]
+
+li.append(10)
+
+print(li)
